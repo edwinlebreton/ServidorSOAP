743b636edc0775d560cfe57dfc2170e1d41dbe51
led0764 <edwinlebreton64@gmail.com>
2019-04-22 22:35:28 +0200

    Servidor fase 2

---
+++ src/java/org/me/modelo/Video.java
@@ -0,0 +1,195 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.modelo;
+
+import org.me.conexion.JdbcDerbyConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.*;
+import java.util.Random;
+
+/**
+ *
+ * @author edwinlebreton
+ */
+public class Video {
+    private int id;
+    private String title;
+    private String author;
+    private String date;
+    private String duration;
+    private int reproductions;
+    private String description;
+    private String format;
+    
+    public Video() {
+    }
+    
+    public Video(String title, String author, String date, String duration,
+            int reproductions, String description,
+            String format) {
+        
+        this.id = generateRandom();
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.duration = duration;
+        this.reproductions = reproductions;
+        this.description = description;
+        this.format = format;
+    }
+    
+    public Video(int id, String title, String author, String date, String duration,
+            int reproductions, String description,
+            String format) {
+        
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.duration = duration;
+        this.reproductions = reproductions;
+        this.description = description;
+        this.format = format;
+    }
+    
+    private int generateRandom(){
+        Random rand = new Random();
+        int n = rand.nextInt(50000);
+        while(randomExists(n)){
+            n=rand.nextInt(50000);
+        }
+        return n;
+    }
+    
+    private boolean randomExists(int n){
+        Connection connection = JdbcDerbyConnection.ConexionDB();
+        
+        String sql = "select * from videos where id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, Integer.toString(n));
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                return true;
+            }
+            pstmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        return false;
+    }
+    
+    public boolean alreadyExists(){
+        Connection connection = JdbcDerbyConnection.ConexionDB();
+        
+        String sql = "select * from videos where titulo = ?";
+ 
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, this.title);
+            ResultSet rs = pstmt.executeQuery();
+            boolean check = false;
+            if(rs.next()){
+                check=true;
+            }
+            pstmt.close();
+            return check;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        return false;
+    }
+    
+    public boolean addVideo(){
+        Connection connection = JdbcDerbyConnection.ConexionDB();
+        
+        String sql = "INSERT INTO VIDEOS VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, this.id);
+                pstmt.setString(2, this.title);
+                pstmt.setString(3, this.author);
+                pstmt.setString(4, this.duration);
+                pstmt.setInt(5, this.reproductions);
+                pstmt.setString(6, this.description);
+                pstmt.setString(7, this.format);
+                pstmt.setString(8, this.date);
+                pstmt.executeUpdate();
+                pstmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        
+        return true;
+    }
+
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public void setTitle(String title){
+        this.author = title;
+    }
+    
+    public String getAuthor(){
+        return author;
+    }
+    
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    
+    public String getDate(){
+        return date;
+    }
+    
+    public void setDate(String date){
+        this.date = date;
+    }
+    
+    public String getDuration(){
+        return duration;
+    }
+    
+    public void setDuration(String duration){
+        this.duration = duration;
+    }
+    
+    public int getReproductions(){
+        return reproductions;
+    }
+    
+    public void setReproductions(int reproductions){
+        this.reproductions = reproductions;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
+    public String getFormat(){
+        return format;
+    }
+    
+    public void setFormat(String format){
+        this.format = format;
+    }
+}
+
---
+++ src/java/org/me/video/VideoWS.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.video;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+import org.me.conexion.JdbcDerbyConnection;
+import org.me.modelo.Video;
+
+/**
+ *
+ * @author edwinlebreton
+ */
+@WebService(serviceName = "VideoWS")
+@Stateless()
+public class VideoWS {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "findByTitle")
+    public List<Video> findByTitle(@WebParam(name = "title") String title) {
+        Connection connection = JdbcDerbyConnection.ConexionDB();
+        List<Video> list= new ArrayList<Video>();
+        String sql = "select * from videos where titulo = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, title);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                int id= rs.getInt("id");
+                String author= rs.getString("autor");
+                String duration = rs.getTime("duracion").toString();
+                int reproductions = rs.getInt("reproducciones");
+                String description = rs.getString("descripcion");
+                String format = rs.getString("formato");
+                String creationDate = rs.getDate("fecha_creacion").toString();
+                Video newVid = new Video(id, title, author, creationDate, 
+                        duration, reproductions, description, format);
+                list.add(newVid);
+            }
+            pstmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        
+        return list;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "findByAuthor")
+    public List<Video> findByAuthor(@WebParam(name = "author") String author) {
+        Connection connection = JdbcDerbyConnection.ConexionDB();
+        List<Video> list= new ArrayList<Video>();
+        String sql = "select * from videos where autor = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, author);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                int id= rs.getInt("id");
+                String title= rs.getString("titulo");
+                String duration = rs.getTime("duracion").toString();
+                int reproductions = rs.getInt("reproducciones");
+                String description = rs.getString("descripcion");
+                String format = rs.getString("formato");
+                String creationDate = rs.getDate("fecha_creacion").toString();
+                Video newVid = new Video(id, title, author, creationDate, 
+                        duration, reproductions, description, format);
+                list.add(newVid);
+            }
+            pstmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        
+        return list;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "findByDate")
+    public List<Video> findByDate(@WebParam(name = "date") String date) {
+        Connection connection = JdbcDerbyConnection.ConexionDB();
+        List<Video> list= new ArrayList<Video>();
+        String sql = "select * from videos where fecha_creacion = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, date);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                int id= rs.getInt("id");
+                String title= rs.getString("titulo");
+                String author= rs.getString("autor");
+                String duration = rs.getTime("duracion").toString();
+                int reproductions = rs.getInt("reproducciones");
+                String description = rs.getString("descripcion");
+                String format = rs.getString("formato");
+                String creationDate = Date.valueOf(date).toString();
+                Video newVid = new Video(id, title, author, creationDate, 
+                        duration, reproductions, description, format);
+                list.add(newVid);
+            }
+            pstmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        
+        return list;
+    }
+}
